8718674e01b059d674d39934ee9727dde0efcf30
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-02-07 22:08:05 -0500

    v1.11

--- app/assets/javascripts/art.js
+++ app/assets/javascripts/art.js
@@ -1,11 +1,8 @@
-$(document).on('mouseover','.art',function(e){
-  var x = $(this).attr('id').slice(-1);
-  $('#art-text'+x).fadeIn().css({"display":"block"});
-});
-$(document).on('mouseout','.art',function(e){
-  var x = $(this).attr('id').slice(-1);
-  $('#art-text'+x).css({"display":"none"});
-});
-$(document).on('click','.art',function(){
-  $(this).css({"border":"2px solid red"});
-});
+// 
+// $(document).on('mouseout','.art',function(e){
+//   var x = $(this).attr('id').slice(-1);
+//   $('#art-text'+x).css({"display":"none"});
+// });
+// // $(document).on('click','.art',function(){
+// //   $(this).css({"border":"2px solid red"});
+// // });
