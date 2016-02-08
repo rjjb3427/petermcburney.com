8dd9d732a70174118648aebac9cd179df7b25f7b
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-02-07 22:08:05 -0500

    v1.11

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,7 +1,7 @@
-var numImages = 3;
-var x = 0;
-setInterval(function() {
-  if (x == numImages){x = 0;}
-  document.getElementById('background').src="/assets/home-image-" + x + ".jpg";
-  x+=1;
-}, 20000);
+// var numImages = 3;
+// var x = 0;
+// setInterval(function() {
+//   if (x == numImages){x = 0;}
+//   document.getElementById('background').src="/assets/home-image-" + x + ".jpg";
+//   // x+=1;
+// }, 20000);
