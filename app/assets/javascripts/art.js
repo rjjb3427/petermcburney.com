a43e24014dfd26d5ec07ab91b27a3bc0b8fd7d7b
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-02-15 20:42:34 -0500

    test

--- app/assets/javascripts/art.js
+++ app/assets/javascripts/art.js
@@ -1,3 +1,5 @@
+var id = 0;
+var numArt = 16;
 
 $(document).on('mouseover','.art',function(e){
   $(this).find('#art-text').css({"display":"block"});
@@ -5,30 +7,26 @@ $(document).on('mouseover','.art',function(e){
 $(document).on('mouseout','.art',function(e){
   $(this).find('#art-text').css({"display":"none"});
 });
-$(document).on('click','#blur',function(e){
-  $('#img-active').css({
-    "display": "none"
-  });
-  $('#blur').css({
-    "display": "none"
-  });
+$(document).on('click','#x',function(e){
+  $('#img-active').css({"display": "none"});
+  $('#blur').css({"display": "none"});
 });
 
 $(document).on('click','.art',function(e){
-  if($('#img-active').css("display") == "block"){
-    $('#img-active').css({
-      "display": "none"
-    });
-    $('#blur').css({
-      "display": "none"
-    });
-  }else{
-    $("#img-active").attr('src', $(this).find("img").attr('src'));
-    $('#img-active').css({
-      "display": "block"
-    });
-    $('#blur').css({
-      "display": "block"
-    });
-  }
+  id = parseInt($(this).attr('id').slice(3));
+  $("#img-active").attr('src', $(this).find("img").attr('src'));
+  $('#img-active').css({"display": "block"});
+  $('#blur').css({"display": "block"});
+});
+
+/* ARROW FUNCTIONALITY */
+$(document).on('click','#arrow-right',function(e){
+  id += 1;
+  if(id == numArt){id = 0;}
+  $("#img-active").attr('src', $('#art' + id).find("img").attr('src'));
+});
+$(document).on('click','#arrow-left',function(e){
+  id -= 1;
+  if(id == -1){id = numArt - 1;}
+  $("#img-active").attr('src', $('#art' + id).find("img").attr('src'));
 });
