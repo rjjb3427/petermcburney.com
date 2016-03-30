e232848f64e43a1ae35e0cf8f95b31a668947963
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-03-29 20:43:12 -0400

    Added static pages for artwork/ 'copic', 'paintings', 'sketches'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :artwork do
-    resources :paintings, :sketches, :copic
-  end
+  get 'artwork/copic'
+  get 'artwork/paintings'
+  get 'artwork/sketches'
+
+  resources :artwork
 
   resources :photos, :profile, :contact, :commissions
 
